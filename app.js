03f2a33ae1e941ad6bdbfacfe40f933d83d24bbe
Nixon <nixon.p@mosaicdigi.com>
2021-12-07 18:37:24 +0800

    connected to test api

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const { default: axios } = require("axios");
 const express = require("express");
 const app = express();
+const routes = require("./routes/routes.js");
+const cors = require("cors");
 const port = 3030;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors({ origin: "http://localhost:3001" }));
+
+routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ routes/routes.js
@@ -0,0 +1,64 @@
+const { default: axios } = require("axios");
+
+const apiURL = (route) => `https://xi.test.network${route}`;
+
+const appRouter = function (app) {
+  app.get("/", (req, res) => {
+    res.status(200).send("Hello World!");
+  });
+
+  app.get("/transactions/limit/:limit", async (req, res) => {
+    const limit = req.params.limit || 10;
+    const response = await axios.get(apiURL("/blocks"));
+    const blocksList = response.data;
+    const transactionList = [];
+    blocksList.forEach((block) => {
+      block.transactions.forEach((transaction) => {
+        transaction.block = block.height;
+      });
+      transactionList.push(...block.transactions);
+    });
+    const transactions = transactionList
+      .slice(0, limit)
+      .sort((a, b) => b.timestamp - a.timestamp);
+
+    return res.status(200).send({
+      count: transactions.length,
+      transactions,
+    });
+  });
+
+  app.get("/blocks", async (req, res) => {
+    const response = await axios.get(apiURL("/blocks"));
+    let blocks = response.data.sort((a, b) => b.height - a.height);
+    blocks.forEach((block) => {
+      block.totalBlockValue = block.transactions
+        .map((transaction) => transaction.value)
+        .reduce((a, b) => a + b, 0);
+    });
+
+    return res.status(200).send({
+      count: blocks.length,
+      blocks,
+    });
+  });
+
+  app.get("/block/:height", async (req, res) => {
+    const height = req.params.height;
+
+    let response = await axios.get(apiURL(`/blocks/${height}`));
+
+    return res.status(200).send(response.data);
+  });
+
+  app.get("/wallets/count", async (req, res) => {
+    const response = await axios.get(apiURL("/wallets"));
+    const wallets = response.data;
+    const count = wallets.length;
+    return res.status(200).send({
+      count,
+    });
+  });
+};
+
+module.exports = appRouter;
