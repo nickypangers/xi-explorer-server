3fdfbdd4b84c03e990933421a4dffc2022f7b580
Nixon <nixon.p@mosaicdigi.com>
2021-12-08 19:28:48 +0800

    implementing try catch for routes

--- routes/routes.js
+++ routes/routes.js
@@ -9,23 +9,27 @@ const appRouter = function (app) {
 
   app.get("/transactions/limit/:limit", async (req, res) => {
     const limit = req.params.limit || 10;
-    const response = await axios.get(apiURL("/blocks"));
-    const blocksList = response.data;
-    const transactionList = [];
-    blocksList.forEach((block) => {
-      block.transactions.forEach((transaction) => {
-        transaction.block = block.height;
+    try {
+      const response = await axios.get(apiURL("/blocks"));
+      const blocksList = response.data;
+      const transactionList = [];
+      blocksList.forEach((block) => {
+        block.transactions.forEach((transaction) => {
+          transaction.block = block.height;
+        });
+        transactionList.push(...block.transactions);
       });
-      transactionList.push(...block.transactions);
-    });
-    const transactions = transactionList
-      .slice(0, limit)
-      .sort((a, b) => b.timestamp - a.timestamp);
+      const transactions = transactionList
+        .slice(0, limit)
+        .sort((a, b) => b.timestamp - a.timestamp);
 
-    return res.status(200).send({
-      count: transactions.length,
-      transactions,
-    });
+      return res.status(200).send({
+        count: transactions.length,
+        transactions,
+      });
+    } catch (e) {
+      return res.status(e.status).send(e);
+    }
   });
 
   app.get("/height", async (req, res) => {
@@ -51,26 +55,92 @@ const appRouter = function (app) {
   });
 
   app.get("/block/:height", async (req, res) => {
-    const height = req.params.height;
+    const heightParam = req.params.height;
 
-    let response = await axios.get(apiURL(`/blocks/${height}`));
+    const height = parseInt(heightParam);
 
-    return res.status(200).send(response.data);
+    if (isNaN(height)) {
+      return res
+        .status(404)
+        .send({ error: true, message: "Please enter valid height" });
+    }
+
+    let blockResponse = await axios.get(apiURL(`/blocks/${height}`));
+
+    let block = blockResponse.data;
+
+    block.transactions.forEach((transaction) => {
+      transaction.block = block.height;
+    });
+
+    return res.status(200).send(block);
   });
 
   app.get("/wallets/count", async (req, res) => {
-    const response = await axios.get(apiURL("/wallets"));
-    const wallets = response.data;
-    const count = wallets.length;
-    return res.status(200).send({
-      count,
-    });
+    try {
+      const response = await axios.get(apiURL("/wallets"));
+      const wallets = response.data;
+      const count = wallets.length;
+      return res.status(200).send({
+        count,
+      });
+    } catch (e) {
+      return res.status(e).send({
+        message: e.message,
+      });
+    }
   });
 
-  app.get("/wallets/:address", async (req, res) => {
+  app.get("/block/:height/transaction/:hash", async (req, res) => {
+    const height = req.params.height;
+    const hash = req.params.hash;
+    try {
+      const transactionResponse = await axios.get(
+        apiURL(`/blocks/${height}/transactions/${hash}`)
+      );
+
+      const transaction = transactionResponse.data;
+
+      return res.status(200).send(transaction);
+    } catch (e) {
+      return res.status(404).send({
+        message: e.message,
+      });
+    }
+  });
+
+  app.get("/wallet/:address", async (req, res) => {
     const address = req.params.address;
-    const response = await axios.get(apiURL(`/wallets/${address}`));
-    return res.status(200).send(response.data);
+    try {
+      const walletResponse = await axios.get(apiURL(`/wallets/${address}`));
+      let wallet = walletResponse.data;
+      wallet.testing = 0;
+
+      let transactionList = [];
+
+      const blocksResponse = await axios.get(apiURL("/blocks"));
+      blocksResponse.data.forEach((block) => {
+        let blockTransactionList = block.transactions;
+        blockTransactionList.forEach((transaction) => {
+          transaction.block = block.height;
+        });
+        transactionList.push(
+          ...blockTransactionList.filter(
+            (transaction) =>
+              transaction.from === address || transaction.to === address
+          )
+        );
+      });
+
+      wallet.transactionList = transactionList;
+      wallet.transactionCount = transactionList.length;
+
+      return res.status(200).send(wallet);
+    } catch (e) {
+      return res.status(404).send({
+        message: e.message,
+      });
+    }
   });
 };
 
